179618f91761ac7f93a563df2c1aa985da7369b5
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-16 16:58:16 +0100

    Added Login and Register functionality

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,42 @@
-import Login from "../components/Login/Login";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import Form from "../components/Form/Form";
 
 const LoginPage = () => {
-    return <Login />;
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:4000/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
+            //save cookie inside react app
+            credentials: "include",
+        });
+        if (response.ok) {
+            setRedirect(true);
+        } else {
+            alert("Wrong credentials");
+        }
+    };
+
+    if (redirect) {
+        return <Navigate to={"/"} />;
+    }
+
+    return (
+        <Form
+            title="Login"
+            onSubmit={handleLogin}
+            username={username}
+            password={password}
+            onChangeUsername={(e) => setUserName(e.target.value)}
+            onChangePassword={(e) => setPassword(e.target.value)}
+        />
+    );
 };
 
 export default LoginPage;
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,34 @@
-import Register from "../components/Register/Register";
+import { useState } from "react";
+import Form from "../components/Form/Form";
 
 const RegisterPage = () => {
-    return <Register />;
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:4000/register", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.status === 200) {
+            alert("registration succesful");
+        } else {
+            alert("registration failed");
+        }
+    };
+
+    return (
+        <Form
+            title="Register"
+            onSubmit={handleRegister}
+            username={username}
+            password={password}
+            onChangeUsername={(e) => setUserName(e.target.value)}
+            onChangePassword={(e) => setPassword(e.target.value)}
+        />
+    );
 };
 
 export default RegisterPage;
