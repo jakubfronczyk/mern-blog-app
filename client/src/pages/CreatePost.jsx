b1e1c9de90604683ac4581c9e1726c2b0e69d45f
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-17 18:57:29 +0100

    Added displaying post from db

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import AddPost from "../components/AddPost/AddPost";
 
 const CreatePost = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [content, setContent] = useState("");
+    const [redirect, setRedirect] = useState(false);
+    // to have access to the file we need to create stata
+    const [files, setFiles] = useState("");
+
+    const handleNewPost = async (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+        data.set("title", title);
+        data.set("description", description);
+        data.set("content", content);
+        data.set("img", files[0]);
+
+        const response = await fetch("http://localhost:4000/post", {
+            method: "POST",
+            //because of file, it easier to send all this information not as json but as formdata
+            body: data,
+            credentials: "include",
+        });
+        if (response.ok) {
+            setRedirect(true);
+        }
+    };
+
+    if (redirect) {
+        return <Navigate to={"/"} />;
+    }
+
     return (
         <>
             <h1>Here you create your new post</h1>
-            <AddPost />
+            <AddPost
+                title={title}
+                description={description}
+                content={content}
+                onSubmit={handleNewPost}
+                onChangeTitle={(e) => setTitle(e.target.value)}
+                onChangeDescription={(e) => setDescription(e.target.value)}
+                onChangeFile={(e) => setFiles(e.target.files)}
+                onChangeContent={(newValue) => setContent(newValue)}
+            />
         </>
     );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,18 @@
+import { useEffect, useState } from "react";
 import Post from "../components/Post/Post";
 
 const Homepage = () => {
-    return (
-        <>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-        </>
-    );
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/post").then((response) => {
+            response.json().then((posts) => {
+                setPosts(posts);
+            });
+        });
+    }, []);
+
+    return <>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</>;
 };
 
 export default Homepage;
