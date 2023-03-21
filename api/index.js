6cc802dffa9ec73184e3583790c4542baa03210a
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-21 17:11:05 +0100

    Added edit endpoint

--- api/index.js
+++ api/index.js
@@ -98,6 +98,39 @@ app.post("/post", uploadMiddleware.single("img"), async (req, res) => {
     });
 });
 
+//edit post
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split(".");
+        const ext = parts[parts.length - 1];
+        newPath = path + "." + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, description, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor =
+            JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+
+        if (!isAuthor) {
+            return res.status(400).json("you are not the author");
+        }
+        await postDoc.updateOne({
+            title,
+            description,
+            content,
+            img: newPath ? newPath : postDoc.cover,
+        });
+
+        res.json(postDoc);
+    });
+});
+
 //get request on post
 app.get("/post", async (req, res) => {
     const posts = await Post.find()
@@ -105,6 +138,13 @@ app.get("/post", async (req, res) => {
         .sort({ createdAt: -1 })
         .limit(20);
     res.json(posts);
+});
+
+//get single post
+app.get("/post/:id", async (req, res) => {
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate("author", ["username"]);
+    res.json(postDoc);
 });
 
 app.listen(4000);
