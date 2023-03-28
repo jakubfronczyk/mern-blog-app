ef8a05f6f61da66c60ea8779f14c11c2ad3bf7bd
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-28 12:52:02 +0200

    Added AWS S3 bucket for img

--- api/index.js
+++ api/index.js
@@ -8,25 +8,52 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require("multer");
-const uploadMiddleware = multer({ dest: "uploads/" });
+const uploadMiddleware = multer({ dest: "/tmp" });
 const fs = require("fs");
+const dotenv = require("dotenv");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdsdvfdkhgjASDvfdDf312CAS2G$$#wqd@";
+const bucket = "kordylas-blog-app";
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" })); //if use credeintals you have to specifiy more info
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname + "/uploads"));
 
-//connect to db
-mongoose.connect(
-    "mongodb+srv://admin:sczXHmLHjLlebe80@cluster0.gvdiog7.mongodb.net/?retryWrites=true&w=majority"
-);
+dotenv.config({ path: "../.env" });
+
+// //connect to db
+// mongoose.connect(process.env.MONGO_URL);
+
+//update files to aws s3 bucket
+async function uploadToS3(path, originalFileName, mimetype) {
+    const client = new S3Client({
+        region: "eu-north-1",
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+    });
+    const parts = originalFileName.split(".");
+    const ext = parts[parts.length - 1];
+    const newFileName = Date.now() + "." + ext;
+    const data = await client.send(
+        new PutObjectCommand({
+            Bucket: bucket,
+            Body: fs.readFileSync(path),
+            Key: newFileName,
+            ContentType: mimetype,
+            ACL: "public-read",
+        })
+    );
+    return `https://${bucket}.s3.amazonaws.com/${newFileName}`;
+}
 
 //this is async function so have to use async and await
 app.post("/register", async (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     const { username, password } = req.body;
     try {
         const userDoc = await User.create({
@@ -42,6 +69,7 @@ app.post("/register", async (req, res) => {
 
 //login authentication
 app.post("/login", async (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     const { username, password } = req.body;
     const userDoc = await User.findOne({ username });
     const passwordCheck = bcrypt.compareSync(password, userDoc.password);
@@ -63,6 +91,7 @@ app.post("/login", async (req, res) => {
 
 //veryfi user
 app.get("/profile", (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     const { token } = req.cookies;
     if (token) {
         jwt.verify(token, secret, {}, (err, info) => {
@@ -80,16 +109,14 @@ app.get("/profile", (req, res) => {
 
 //logut user, reset cookie
 app.post("/logout", (req, res) => {
-    res.cookie("token", "").json("ok");
+    res.cookie("token", "").json(true);
 });
 
 //create post
 app.post("/post", uploadMiddleware.single("img"), async (req, res) => {
-    const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const extension = parts[parts.length - 1];
-    const newPath = path + "." + extension;
-    fs.renameSync(path, newPath);
+    mongoose.connect(process.env.MONGO_URL);
+    const { originalname, path, mimetype } = req.file;
+    const url = await uploadToS3(path, originalname, mimetype);
     //to take author id
     const { token } = req.cookies;
     jwt.verify(token, secret, {}, async (err, info) => {
@@ -99,7 +126,7 @@ app.post("/post", uploadMiddleware.single("img"), async (req, res) => {
             title,
             description,
             content,
-            img: newPath,
+            img: url,
             author: info.id,
         });
         res.json(postDoc);
@@ -108,13 +135,11 @@ app.post("/post", uploadMiddleware.single("img"), async (req, res) => {
 
 //edit post
 app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     let newPath = null;
     if (req.file) {
-        const { originalname, path } = req.file;
-        const parts = originalname.split(".");
-        const ext = parts[parts.length - 1];
-        newPath = path + "." + ext;
-        fs.renameSync(path, newPath);
+        const { originalname, path, mimetype } = req.file;
+        newPath = await uploadToS3(path, originalname, mimetype);
     }
 
     const { token } = req.cookies;
@@ -131,7 +156,7 @@ app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
             title,
             description,
             content,
-            cover: newPath ? newPath : postDoc.cover,
+            img: newPath ? newPath : postDoc.img,
         });
 
         res.json(postDoc);
@@ -140,6 +165,7 @@ app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
 
 //get request on post
 app.get("/post", async (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     const posts = await Post.find()
         .populate("author", ["username"])
         .sort({ createdAt: -1 })
@@ -149,11 +175,10 @@ app.get("/post", async (req, res) => {
 
 //get single post
 app.get("/post/:id", async (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     const { id } = req.params;
     const postDoc = await Post.findById(id).populate("author", ["username"]);
     res.json(postDoc);
 });
 
 app.listen(4000);
-//mongodb+srv://admin:sczXHmLHjLlebe80@cluster0.gvdiog7.mongodb.net/?retryWrites=true&w=majority
-//
